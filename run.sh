f2bf312730bf46e33ec050251b49a45723a08a23
Egor A. Fisher <efisher@griddynamics.com>
2021-09-27 21:11:25 +0300

    -

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-#export ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_FORCE_COLOR=True
-
-#ansible-playbook ${pb_path} -i ${inventory} -u ${GD_DEPLOY_USER} --key-file=${GD_DEPLOY_KEY} --extra-vars "bootstrap_user=${GD_DEPLOY_USER}"
-            
-
 ansible-playbook -i hosts -u jagular --key-file=~jagular/.ssh/id_rsa --ask-vault-pass $1
