308a4d527270493d54bd7523130d9e97dc97ac56
efisher <ef@>
2022-09-07 20:45:35 +0300

    -

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export ANSIBLE_FORCE_COLOR=True
-ansible-playbook -i hosts -u jagular --key-file=~jagular/.ssh/id_rsa --ask-vault-pass $1
+ansible-playbook -i hosts -u jagular --key-file=~jagular/.ssh/id_rsa --ask-vault-pass "$@"
